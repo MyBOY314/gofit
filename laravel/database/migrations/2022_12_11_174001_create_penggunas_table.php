3981e86c50949ce69e752938271578ee7335a5c8
Dave <davehabelpaprindey@gmail.com>
2022-12-18 19:01:50 +0700

    Penulis sudah jadi, perlukan created dan updated

--- laravel/database/migrations/2022_12_11_174001_create_penggunas_table.php
+++ laravel/database/migrations/2022_12_11_174001_create_penggunas_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('pengguna', function (Blueprint $table) {
+        Schema::create('penggunas', function (Blueprint $table) {
             $table->id();
 
             $table->string('nama');
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('pengguna');
+        Schema::dropIfExists('penggunas');
     }
 };
